df9a8de75e0364752218676f8b654992c3fa981d
imshiva17 <shivarajak277@gmail.com>
2025-04-20 23:39:18 +0530

    there is some fault

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ gsap.from(".card", {
   scrollTrigger: {
     trigger: ".card",
     scroller: "body",
-    markers: true,
+    markers: false,
     start: "top 70%",
     end: "top 65%",
     scrub: 1,
